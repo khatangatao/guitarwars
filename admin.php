0c4af8445f520c816a1af8049cdfd8755a65caf0
Evgeniy Ovchinnikov <khatangatao@gmail.com>
2017-05-08 18:43:05 +0700

    Аутентификация пользватель вынесена в отдельный файл. В скрипты удаления данных из БД добавлена команда require_once, которая подключает файл аутентификации

--- admin.php
+++ admin.php
@@ -1,17 +1,5 @@
 <?php
-//Имя пользователя и пароль для аутентификации
-$username = 'rock';
-$password = 'roll';
-
-if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) ||
-	($_SERVER['PHP_AUTH_USER'] != $username) || 
-	($_SERVER['PHP_AUTH_PW'] != $password)) {
-	//Имя пользователя/пароль не действительны для отправки HTTP-заголовков,
-	//подтверждающих аутентификацию
-	header('HTTP/1.1 401 Unauthorized');
-	header('WWW-Authenticate:Basic realm="Гитарные войны"');
-	exit ('<h2>Гитарные войны <h2>Извините, вы должны ввести правильные имя пользователя и пароль, чтобы получить доступ к этой странице.');
-}
+require_once('authorize.php');
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
