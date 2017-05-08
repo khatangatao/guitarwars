807079ea70a6733c2ff2b372741741a43b61b503
Evgeniy Ovchinnikov <khatangatao@gmail.com>
2017-05-08 18:33:27 +0700

    Сделана HTTP-аутентификация

--- admin.php
+++ admin.php
@@ -1,5 +1,31 @@
 <?php
+//Имя пользователя и пароль для аутентификации
+$username = 'rock';
+$password = 'roll';
 
+if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) ||
+	($_SERVER['PHP_AUTH_USER'] != $username) || 
+	($_SERVER['PHP_AUTH_PW'] != $password)) {
+	//Имя пользователя/пароль не действительны для отправки HTTP-заголовков,
+	//подтверждающих аутентификацию
+	header('HTTP/1.1 401 Unauthorized');
+	header('WWW-Authenticate:Basic realm="Гитарные войны"');
+	exit ('<h2>Гитарные войны <h2>Извините, вы должны ввести правильные имя пользователя и пароль, чтобы получить доступ к этой странице.');
+}
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Гитарные войны. Панель администратора</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+  <h2>Гитарные войны. Панель администратора</h2>
+
+<?php
 require_once('appvars.php');
 require_once('connectvars.php');
 
@@ -18,7 +44,7 @@ while ($row = mysqli_fetch_array($data)) {
 	echo '<tr class="scorerow"><td><strong>' . $row['name'] . '</strong><td>';
 	echo '<td>' . $row['date'] . '</td>';
 	echo '<td>' . $row['score'] . '</td>';
-	echo '<td><a href="removescore.php?id=' . $row['id'] . '&amp;score=' . $row['score'] . '&amp;screenshot=' . $row['screenshot'] . '">Удалить</a></td></tr>';
+	echo '<td><a href="removescore.php?id=' . $row['id'] . '&amp;date=' . $row['date'] . '&amp;name=' . $row['name'] . '&amp;score=' . $row['score'] . '&amp;screenshot=' . $row['screenshot'] . '">Удалить</a></td></tr>';
 }
 
 echo '</table>';
